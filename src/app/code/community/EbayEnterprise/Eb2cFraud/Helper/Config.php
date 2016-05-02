5dd3150d8b8d311f150377d67df9e103e483fefe
root <root@mag-sandbox09.sandbox.gspt.net>
2016-05-02 09:13:03 -0400

    fixed config helper

--- src/app/code/community/EbayEnterprise/Eb2cFraud/Helper/Config.php
+++ src/app/code/community/EbayEnterprise/Eb2cFraud/Helper/Config.php
@@ -20,22 +20,13 @@
  */
 class EbayEnterprise_RiskService_Helper_Config
 {
-	const ENABLED = 'ebayenterprise_riskinsight/risk_insight/enabled';
-	const STORE_ID = 'ebayenterprise_riskinsight/risk_insight/store_id';
-	const API_HOSTNAME = 'ebayenterprise_riskinsight/risk_insight/hostname';
-	const API_KEY = 'ebayenterprise_riskinsight/risk_insight/key';
-	const API_TIMEOUT = 'ebayenterprise_riskinsight/risk_insight/timeout';
-	const DEBUG = 'ebayenterprise_riskinsight/risk_insight/debug';
-	const ORDER_SOURCE = 'ebayenterprise_riskinsight/risk_insight/order_source';
-	const HIGH_RESPONSE_ACTION = 'ebayenterprise_riskinsight/risk_insight/high_action';
-	const MEDIUM_RESPONSE_ACTION = 'ebayenterprise_riskinsight/risk_insight/medium_action';
-	const LOW_RESPONSE_ACTION = 'ebayenterprise_riskinsight/risk_insight/low_action';
-	const UNKNOWN_RESPONSE_ACTION = 'ebayenterprise_riskinsight/risk_insight/unknown_action';
-	const LANGUAGE_CODE = 'ebayenterprise_riskinsight/risk_insight/language_code';
-	const CARD_TYPE_MAP = 'ebayenterprise_riskinsight/risk_insight/card_type_map';
-	const SHIPPING_METHOD_MAP = 'ebayenterprise_riskinsight/risk_insight/shipping_method_map';
-	const PAYMENT_ADAPTER_MAP = 'ebayenterprise_riskinsight/risk_insight/payment_adapter_map';
-	const FEEDBACK_RESEND_THRESHOLD = 'ebayenterprise_riskinsight/risk_insight/feedback_request_resend_threshold';
+	const ENABLED = 'eb2cfraud/risk_service/enabled';
+	const STORE_ID = 'eb2ccore/general/store_id';
+	const API_HOSTNAME = 'eb2ccore/api/hostname';
+	const API_KEY = 'eb2ccore/api/key';
+	const API_TIMEOUT = 'eb2ccore/api/timeout';
+	const DEBUG = 'eb2cfraud/risk_service/debug';
+	const LANGUAGE_CODE = 'eb2ccore/general/language_code';
 
 	/**
 	 * check if Risk Insight module is enable in the store config
@@ -112,104 +103,5 @@ class EbayEnterprise_RiskService_Helper_Config
 	public function getApiTimeout($store=null)
 	{
 		return Mage::getStoreConfig(static::API_TIMEOUT, $store);
-	}
-
-	/**
-	 * retrieve the order source override setting from store config
-	 *
-	 * @param  mixed
-	 * @return string
-	 */
-	public function getOrderSource($store=null)
-	{
-		return Mage::getStoreConfig(static::ORDER_SOURCE, $store);
-	}
-
-	/**
-	 * retrieve the high response action settings from store config
-	 *
-	 * @param  mixed
-	 * @return string
-	 */
-	public function getHighResponseAction($store=null)
-	{
-		return Mage::getStoreConfig(static::HIGH_RESPONSE_ACTION, $store);
-	}
-
-	/**
-	 * retrieve the medium response action settings from store config
-	 *
-	 * @param  mixed
-	 * @return string
-	 */
-	public function getMediumResponseAction($store=null)
-	{
-		return Mage::getStoreConfig(static::MEDIUM_RESPONSE_ACTION, $store);
-	}
-
-	/**
-	 * retrieve the low response action settings from store config
-	 *
-	 * @param  mixed
-	 * @return string
-	 */
-	public function getLowResponseAction($store=null)
-	{
-		return Mage::getStoreConfig(static::LOW_RESPONSE_ACTION, $store);
-	}
-
-	/**
-	 * retrieve the unknown response action settings from store config
-	 *
-	 * @param  mixed
-	 * @return string
-	 */
-	public function getUnknownResponseAction($store=null)
-	{
-		return Mage::getStoreConfig(static::UNKNOWN_RESPONSE_ACTION, $store);
-	}
-
-	/**
-	 * retrieve the payment method card type map settings from store config
-	 *
-	 * @param  mixed
-	 * @return string
-	 */
-	public function getPaymentMethodCardTypeMap($store=null)
-	{
-		return Mage::getStoreConfig(static::CARD_TYPE_MAP, $store);
-	}
-
-	/**
-	 * retrieve the shipping method map settings from store config
-	 *
-	 * @param  mixed
-	 * @return string
-	 */
-	public function getShippingMethodMap($store=null)
-	{
-		return Mage::getStoreConfig(static::SHIPPING_METHOD_MAP, $store);
-	}
-
-	/**
-	 * retrieve the payment adapter map settings from store config
-	 *
-	 * @param  mixed
-	 * @return string
-	 */
-	public function getPaymentAdapterMap($store=null)
-	{
-		return Mage::getStoreConfig(static::PAYMENT_ADAPTER_MAP, $store);
-	}
-
-	/**
-	 * retrieve the feedback request resend threshold settings from store config
-	 *
-	 * @param  mixed
-	 * @return int
-	 */
-	public function getFeedbackResendThreshold($store=null)
-	{
-		return (int) Mage::getStoreConfig(static::FEEDBACK_RESEND_THRESHOLD, $store);
 	}
 }
